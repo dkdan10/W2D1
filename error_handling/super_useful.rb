1161c1335405c29dc4de9aa3fe29a344085331c1
Daniel <danielkeinan0@gmail.com>
2019-06-17 14:16:05 -0400

    Finish error handling funtime exercise additional commit.

--- error_handling/super_useful.rb
+++ error_handling/super_useful.rb
@@ -4,30 +4,73 @@ def convert_to_int(str)
 end
 
 # PHASE 3
+
+class CoffeeError < StandardError
+end
+
 FRUITS = ["apple", "banana", "orange"]
 
 def reaction(maybe_fruit)
   if FRUITS.include? maybe_fruit
     puts "OMG, thanks so much for the #{maybe_fruit}!"
-  else 
-    raise StandardError 
-  end 
+  elsif maybe_fruit == "coffee"
+    raise CoffeeError
+  else
+    raise StandardError
+  end
 end
 
 def feed_me_a_fruit
   puts "Hello, I am a friendly monster. :)"
-
   puts "Feed me a fruit! (Enter the name of a fruit:)"
-  maybe_fruit = gets.chomp
-  reaction(maybe_fruit) 
+   
+  begin
+    maybe_fruit = gets.chomp
+    reaction(maybe_fruit)
+  rescue CoffeeError
+    p "Coffee is cool, but I want fruit"
+    retry
+  rescue StandardError
+    p "I wanted fruit :("
+  end
+
 end  
 
 # PHASE 4
+class NotBestfriendError < StandardError
+end
+
+class NameEmptyError < StandardError
+end
+
+class PTEmptyError < StandardError
+end
+
 class BestFriend
   def initialize(name, yrs_known, fav_pastime)
-    @name = name
-    @yrs_known = yrs_known
-    @fav_pastime = fav_pastime
+    if yrs_known >= 5
+      @yrs_known = yrs_known
+    else
+      p "Only #{yrs_known} years? Not a REAL friend...."
+      raise NotBestfriendError
+    end 
+
+    if !name.empty?
+      @name = name
+    else
+      p "That isn't a real name!"
+      raise NameEmptyError
+    end 
+
+    if !fav_pastime.empty?
+      @fav_pastime = fav_pastime
+    else
+      p "Fake activity..."
+      raise PTEmptyError
+    end 
+
+    
+
   end
 
   def talk_about_friendship
--- error_handling/user_script.rb
+++ error_handling/user_script.rb
@@ -1,10 +1,17 @@
 require_relative 'super_useful'
+input = "five"
+begin
+  puts "'five' == #{convert_to_int(input)}"
+rescue ArgumentError
+  puts "Please enter a valid integer"
+  input = gets.chomp
+  retry
+end
 
-puts "'five' == #{convert_to_int('five')}"
 
 feed_me_a_fruit
 
-sam = BestFriend.new('', 1, '')
+sam = BestFriend.new('Daniel', 5, 'Skiing')
 
 sam.talk_about_friendship
 sam.do_friendstuff
