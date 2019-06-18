c787222dfc883af8b9ebd82a2d7326597f4608ec
Daniel <danielkeinan0@gmail.com>
2019-06-18 18:12:36 -0400

    Fix pawn move selection

--- chess_game/pieces/pawn.rb
+++ chess_game/pieces/pawn.rb
@@ -7,24 +7,25 @@ class Pawn < Piece
     if self.color == :black
       if board.valid_pos?([x+1, y+1]) && board[[x+1, y+1]].color == :white
         moves << [x+1, y+1]
-      elsif board.valid_pos?([x+1, y-1]) && board[[x+1, y-1]].color == :white
+      end
+      if board.valid_pos?([x+1, y-1]) && board[[x+1, y-1]].color == :white
         moves << [x+1, y-1]
       end
       moves << [x+1, y] if board.valid_pos?([x+1, y]) && board[[x+1, y]].is_a?(NullPiece)
     else
       if board.valid_pos?([x-1, y-1]) && board[[x-1, y-1]].color == :black
         moves << [x-1, y-1]
-        
-      elsif board.valid_pos?([x-1, y+1]) && board[[x-1, y+1]].color == :black
+      end
+      if board.valid_pos?([x-1, y+1]) && board[[x-1, y+1]].color == :black
         moves << [x-1, y+1]
       end
       moves << [x-1, y] if board.valid_pos?([x-1, y]) && board[[x-1, y]].is_a?(NullPiece)
     end
 
     #takes care of second step
-    if x == 1 #black pawns 
+    if x == 1 && self.color == :black #black pawns 
       moves.push([x+2, y]) if board[[x+1,y]].is_a?(NullPiece) && board[[x+2,y]].is_a?(NullPiece) 
-    elsif x == 6 #white pawns
+    elsif x == 6 && self.color == :white #white pawns
       moves.push([x-2, y]) if board[[x-1,y]].is_a?(NullPiece) && board[[x-2,y]].is_a?(NullPiece)
     end
 
